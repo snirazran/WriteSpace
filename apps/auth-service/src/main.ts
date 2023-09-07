e2340ea19b008bde5a986e7910933d338eebf094
Snir Azran <snirazran1@gmail.com>
2023-09-07 20:22:22 +0300

    ih

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -7,9 +7,11 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
   });
-
   const config = new DocumentBuilder()
     .setTitle('Auth Service')
     .setDescription('Auth Service api documentation')
