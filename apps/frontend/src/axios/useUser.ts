bdcd51330fa1cb50eb82a93a206e0780049522aa
Snir Azran <snirazran1@gmail.com>
2023-09-07 14:29:15 +0300

    wha

--- apps/frontend/src/axios/useUser.ts
+++
@@ -1,6 +0,0 @@
-import { useLocalStorage } from '../utils/useLocalStorage';
-import { User } from '../utils/user';
-
-export const useUser = () => {
-  return useLocalStorage<User | null>('user', null);
-};
